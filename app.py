3149ae3227222056f668e241d320a3cfd04496d4
carlosn95 <gordillocarlosn@gmail.com>
2026-03-09 02:28:58 -0300

    changed app and index.html for debug

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify, render_template
 import os
+import traceback
 from flask_cors import CORS, cross_origin
 from cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.prediction import PredictionPipeline
 
-os.putenv('LANG','en_US.UTF-8')
-os.putenv('LC_ALL','en_US.ITF-8')
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
 CORS(app)
@@ -14,27 +15,43 @@ class ClientApp:
     def __init__(self):
         self.filename = 'inputImage.jpg'
         self.classifier = PredictionPipeline(self.filename)
-        
-@app.route('/',methods = ['GET'])
+
+clApp = ClientApp()
+
+@app.route('/', methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
 
-@app.route('/train', methods = ['GET','POST'])
+@app.route('/train', methods=['GET', 'POST'])
 @cross_origin()
 def trainRoute():
     os.system('python main.py')
     return 'Training done successfully!'
 
-@app.route('/predict', methods = ['POST'])
+@app.route('/predict', methods=['POST'])
 @cross_origin()
 def predictRoute():
-    image = request.json['image']
-    decodeImage(image, clApp.filename)
-    result = clApp.classifier.predict()
-    return jsonify(result)
+    try:
+        print("Entró a /predict", flush=True)
+        print("Content-Type:", request.content_type, flush=True)
+        print("request.json existe:", request.json is not None, flush=True)
 
+        image = request.json['image']
+        print("Base64 recibido. Largo:", len(image), flush=True)
+
+        decodeImage(image, clApp.filename)
+        print("Imagen decodificada y guardada en:", clApp.filename, flush=True)
+
+        result = clApp.classifier.predict()
+        print("Resultado de predict():", result, flush=True)
+
+        return jsonify(result)
+
+    except Exception as e:
+        print("ERROR EN /predict:", str(e), flush=True)
+        traceback.print_exc()
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    clApp = ClientApp()
-    app.run(host = '0.0.0.0', port = 8080, debug = True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
